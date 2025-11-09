337d3cb26705c38cd3b55d2d8f7353cc4be2407a
Priyesh <kpriyesh1908@gmail.com>
2025-11-09 12:17:30 +0530

    Add gh-pages deployment script, update dependencies, and configure CNAME for hosting

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css' // Main styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Evershan-Chauffer-website">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
